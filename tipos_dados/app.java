ffb899399b490eaa2a74a23874b1d1767657919e
Guilherme <guif.cavalcante@gmail.com>
2025-09-16 20:01:02 -0500

    feat: Tipos de dados em Java

---
+++ tipos_dados/app.java
@@ -0,0 +1,34 @@
+package tipos_dados;
+
+public class app {
+    public static void main(String[] args) {
+        byte idade = 21;
+        System.out.println("Idade (tipo byte):" + idade);
+        
+        short populacaoCidade = 25000;
+        System.out.println("População da cidade (tipo short):" + populacaoCidade);
+        
+        int populacaoMundial = 78000000;
+        System.out.println("População mundial (tipo int):" + populacaoMundial);
+        
+        long distanciaEstrelas = 9460730472580800L;
+        System.out.println("Distância entre as estrelas (tipo long):" + distanciaEstrelas);
+        
+        float peso = 60.1f;
+        System.out.println("Peso (tipo float):" + peso);
+        
+        double pi = 3.141592653589793;
+        System.out.println("Valor de PI (tipo double):" + pi);
+        
+        char inicialNome = 'G';
+        System.out.println("Inicial do nome (tipo byte):" + inicialNome);
+        
+        boolean isJavaDivertido = true;
+        boolean isChuvaAamnha = false;
+        System.out.println("Java é divertido? (tipo boolean):" + isJavaDivertido);
+        System.out.println("Vai chover amanhã? (tipo boolean):" + isChuvaAamnha);
+        
+        String nomeCompleto = "Guilherme Felix Cavalcante";
+        System.out.println("Nome completo (tipo string):" + nomeCompleto);
+    }
+}
